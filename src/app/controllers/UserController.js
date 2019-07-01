4ebff91c1d931fba6972d725eabb3d96726ccdab
Igor Trindade <igorhideki@gmail.com>
2019-07-01 05:55:04 -0400

    add verificacao se o usuario ja existe

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,9 +2,22 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const { email } = req.body;
+    const userExists = await User.findOne({ where: { email } });
+
+    if (userExists) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
     const user = await User.create(req.body);
 
     return res.json(user);
+  }
+
+  async update(req, res) {
+    console.log(req.headers.authorization);
+
+    return res.json({ ok: true });
   }
 }
 
