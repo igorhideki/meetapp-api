e47159a97f1f2a9a445248fbd014b9df32303a79
Igor Trindade <igorhideki@gmail.com>
2019-07-25 21:55:07 -0400

    update dados retornados do OrganizingController

--- src/app/controllers/OrganizingController.js
+++ src/app/controllers/OrganizingController.js
@@ -1,10 +1,24 @@
 import Meetup from '../models/Meetup';
+import User from '../models/User';
 
 class OrganizingController {
   async index(req, res) {
     const meetups = await Meetup.findAll({
       where: { user_id: req.userId },
       order: ['date'],
+      attributes: [
+        'title',
+        'description',
+        'location',
+        'date',
+        'past',
+        'banner_id',
+      ],
+      include: {
+        model: User,
+        as: 'user',
+        attributes: ['name', 'email'],
+      },
     });
 
     return res.json(meetups);
